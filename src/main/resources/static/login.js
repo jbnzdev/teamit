4ee9ff9b53447103da494efb1aa104aa7fd345b5
jeybeyen <jeybeyen@gmail.com>
2019-02-26 11:50:45 +0530

    Initial commit

---
+++ src/main/resources/static/login.js
@@ -0,0 +1,18 @@
+new Vue({
+  el: '#app',
+  data: {
+    msg:'Login to  the app',
+    username:'username',
+    password:'password'
+  },
+  methods:{
+	  loginSubmit: function(){
+		  this.msg = "Login clicked";
+		  this.ajaxRequest = true;
+		  axios.post('http://localhost:8080/api/v1/userLogin',{
+			  username:this.username,
+			  password:this.password
+		  });
+	  }
+  }
+});
