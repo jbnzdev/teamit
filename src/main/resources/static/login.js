752d68f35de3c3f6b0bb8db655d3ee32fef2f4bc
jeybeyen <jeybeyen@gmail.com>
2019-02-27 00:42:52 +0530

    Modified te post url for login

--- src/main/resources/static/login.js
+++ src/main/resources/static/login.js
@@ -9,7 +9,7 @@ new Vue({
 	  loginSubmit: function(){
 		  this.msg = "Login clicked";
 		  this.ajaxRequest = true;
-		  axios.post('http://localhost:8080/api/v1/userLogin',{
+		  axios.post('./api/v1/userLogin',{
 			  username:this.username,
 			  password:this.password
 		  });
